783edcc353e951e02765ebf35620846be5ff6eaf
Jaime <jameswhitfield65@gmail.com>
2026-06-28 09:33:55 -0500

    chore(veriloga): regenerate mul-sub rhs helper

--- crates/rspice-core/src/device/veriloga_generated/support.rs
+++ crates/rspice-core/src/device/veriloga_generated/support.rs
@@ -3864,16 +3864,10 @@ impl<const VARIABLE_COUNT: usize, const NODE_COUNT: usize, const BRANCH_COUNT: u
         let left_value = self.v[left];
         let middle_value = self.v[middle];
         let right_value = self.v[right];
-        let left_dn = self.dn[left];
-        let middle_dn = self.dn[middle];
-        let right_dn = self.dn[right];
-        let left_db = self.db[left];
-        let middle_db = self.db[middle];
-        let right_db = self.db[right];
         let difference = middle_value - right_value;
         self.v[index] = left_value * difference;
-        for axis in 0..NODE_COUNT { self.dn[index][axis] = left_dn[axis] * difference + left_value * (middle_dn[axis] - right_dn[axis]); }
-        for axis in 0..BRANCH_COUNT { self.db[index][axis] = left_db[axis] * difference + left_value * (middle_db[axis] - right_db[axis]); }
+        for axis in 0..NODE_COUNT { self.dn[index][axis] = self.dn[left][axis] * difference + left_value * (self.dn[middle][axis] - self.dn[right][axis]); }
+        for axis in 0..BRANCH_COUNT { self.db[index][axis] = self.db[left][axis] * difference + left_value * (self.db[middle][axis] - self.db[right][axis]); }
     }
 
     #[inline]
@@ -18118,16 +18112,10 @@ impl<const VARIABLE_COUNT: usize, const NODE_COUNT: usize, const BRANCH_COUNT: u
         let left_value = self.v[left];
         let middle_value = self.v[middle];
         let right_value = self.v[right];
-        let left_dn = self.dn[left];
-        let middle_dn = self.dn[middle];
-        let right_dn = self.dn[right];
-        let left_db = self.db[left];
-        let middle_db = self.db[middle];
-        let right_db = self.db[right];
         let difference = middle_value - right_value;
         self.v[index] = left_value * difference;
-        for axis in 0..NODE_COUNT { self.dn[index][axis] = left_dn[axis] * difference + left_value * (middle_dn[axis] - right_dn[axis]); }
-        for axis in 0..BRANCH_COUNT { self.db[index][axis] = left_db[axis] * difference + left_value * (middle_db[axis] - right_db[axis]); }
+        for axis in 0..NODE_COUNT { self.dn[index][axis] = self.dn[left][axis] * difference + left_value * (self.dn[middle][axis] - self.dn[right][axis]); }
+        for axis in 0..BRANCH_COUNT { self.db[index][axis] = self.db[left][axis] * difference + left_value * (self.db[middle][axis] - self.db[right][axis]); }
     }
 
     #[inline]
