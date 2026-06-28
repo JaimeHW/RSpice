ea57e14cecc9f011329a97dfb2d473662b88556c
Jaime <jameswhitfield65@gmail.com>
2026-06-28 10:56:08 -0500

    chore(veriloga): regenerate mul-add rhs helper

--- crates/rspice-core/src/device/veriloga_generated/support.rs
+++ crates/rspice-core/src/device/veriloga_generated/support.rs
@@ -3830,16 +3830,10 @@ impl<const VARIABLE_COUNT: usize, const NODE_COUNT: usize, const BRANCH_COUNT: u
         let left_value = self.v[left];
         let middle_value = self.v[middle];
         let right_value = self.v[right];
-        let left_dn = self.dn[left];
-        let middle_dn = self.dn[middle];
-        let right_dn = self.dn[right];
-        let left_db = self.db[left];
-        let middle_db = self.db[middle];
-        let right_db = self.db[right];
         let sum = middle_value + right_value;
         self.v[index] = left_value * sum;
-        for axis in 0..NODE_COUNT { self.dn[index][axis] = left_dn[axis] * sum + left_value * (middle_dn[axis] + right_dn[axis]); }
-        for axis in 0..BRANCH_COUNT { self.db[index][axis] = left_db[axis] * sum + left_value * (middle_db[axis] + right_db[axis]); }
+        for axis in 0..NODE_COUNT { self.dn[index][axis] = self.dn[left][axis] * sum + left_value * (self.dn[middle][axis] + self.dn[right][axis]); }
+        for axis in 0..BRANCH_COUNT { self.db[index][axis] = self.db[left][axis] * sum + left_value * (self.db[middle][axis] + self.db[right][axis]); }
     }
 
     #[inline]
@@ -18063,16 +18057,10 @@ impl<const VARIABLE_COUNT: usize, const NODE_COUNT: usize, const BRANCH_COUNT: u
         let left_value = self.v[left];
         let middle_value = self.v[middle];
         let right_value = self.v[right];
-        let left_dn = self.dn[left];
-        let middle_dn = self.dn[middle];
-        let right_dn = self.dn[right];
-        let left_db = self.db[left];
-        let middle_db = self.db[middle];
-        let right_db = self.db[right];
         let sum = middle_value + right_value;
         self.v[index] = left_value * sum;
-        for axis in 0..NODE_COUNT { self.dn[index][axis] = left_dn[axis] * sum + left_value * (middle_dn[axis] + right_dn[axis]); }
-        for axis in 0..BRANCH_COUNT { self.db[index][axis] = left_db[axis] * sum + left_value * (middle_db[axis] + right_db[axis]); }
+        for axis in 0..NODE_COUNT { self.dn[index][axis] = self.dn[left][axis] * sum + left_value * (self.dn[middle][axis] + self.dn[right][axis]); }
+        for axis in 0..BRANCH_COUNT { self.db[index][axis] = self.db[left][axis] * sum + left_value * (self.db[middle][axis] + self.db[right][axis]); }
     }
 
     #[inline]
